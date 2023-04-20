af883e1a271366854cf1de93e0c05251b72d1320
ntkc03 <nguyenthikieuchinh2801@gmail.com>
2023-04-20 13:54:03 +0700

    add setting action

--- app/src/main/java/com/example/money_meow/setting/AccountSettings.java
+++ app/src/main/java/com/example/money_meow/setting/AccountSettings.java
@@ -1,5 +1,6 @@
 package com.example.money_meow.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,12 +8,12 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.money_meow.R;
+import com.example.money_meow.home.Home;
+import com.example.money_meow.information.Information;
+import com.example.money_meow.transaction.TransactionAction;
 
 public class AccountSettings extends AppCompatActivity {
-    Button account;
-    Button editAcc;
-    Button logOut;
-    Button display;
+    Button account, editAcc, logOut, display, addTransBtn, infoBtn, historyBtn, searchBtn, settingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,27 @@ public class AccountSettings extends AppCompatActivity {
         logOut = (Button) findViewById(R.id.logout);
         display = (Button) findViewById(R.id.display);
 
+        addTransBtn = findViewById(R.id.AddTransBtn);
+        infoBtn = findViewById(R.id.InfoBtn);
+        historyBtn = findViewById(R.id.HistoryBtn);
+        searchBtn = findViewById(R.id.SearchBtn);
+        settingBtn = findViewById(R.id.SettingBtn);
+
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AccountSettings.this, Information.class);
+                startActivity(intent);
+            }
+        });
+        addTransBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AccountSettings.this, TransactionAction.class);
+                startActivity(intent);
+            }
+        });
+
         account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,6 +53,8 @@ public class AccountSettings extends AppCompatActivity {
                 logOut.setVisibility(View.VISIBLE);
             }
         });
+
+
 
 
     }
