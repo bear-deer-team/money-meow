9f184bfcaf94ae1fd5d8ef95837bc07061b88416
Uyen0408 <21020419@vnu.edu.vn>
2023-04-26 00:43:19 +0700

    fix info ui

--- app/src/main/java/com/example/money_meow/information/Information.java
+++ app/src/main/java/com/example/money_meow/information/Information.java
@@ -11,16 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.money_meow.BaseActivity;
 import com.example.money_meow.R;
 import com.example.money_meow.home.Home;
+import com.example.money_meow.manageEngine.statistic.StatisticsAction;
 import com.example.money_meow.setting.Settings;
+import com.example.money_meow.transaction.TransactionAction;
 
 public class Information extends BaseActivity {
-    private Button  homeBtn ;
+    private Button  homeBtn, historyBtn, searchBtn, settingBtn,addTransBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info);
         homeBtn = (Button) findViewById(R.id.HomeBtn);
+        addTransBtn = findViewById(R.id.AddTransBtn);
+        historyBtn = findViewById(R.id.HistoryBtn);
+        //searchBtn = findViewById(R.id.SearchBtn);
+        settingBtn = findViewById(R.id.SettingBtn);
 
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +36,29 @@ public class Information extends BaseActivity {
             }
         });
 
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Information.this, StatisticsAction.class);
+                startActivity(intent);
+            }
+        });
+
+        addTransBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Information.this, TransactionAction.class);
+                startActivity(intent);
+            }
+        });
+
+        settingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Information.this, Settings.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
